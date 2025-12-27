ef7d41c063f46ef470e2af865b52ad515239f1d1
VarunKumar988 <cvarunkumar455@gmail.com>
2025-12-27 17:30:05 +0530

    fix

--- src/integrations/external-supabase/client.ts
+++ src/integrations/external-supabase/client.ts
@@ -5,7 +5,7 @@ const EXTERNAL_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3M
 
 export const externalSupabase = createClient(EXTERNAL_SUPABASE_URL, EXTERNAL_SUPABASE_ANON_KEY, {
     auth: {
-        storage: sessionStorage,
+        storage: localStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: true
